42e11df20d7fe5f7bd96fdeae0809818c6bae2bb
Charlie Gleason <hi@charliegleason.com>
2023-12-01 14:01:46 +0000

    Tiny tweak to fallback

--- src/components/fallback.tsx
+++ src/components/fallback.tsx
@@ -1,6 +1,6 @@
 function Fallback() {
   return (
-    <div className="fixed font-mono inset-0 grid place-content-center space-y-8 text-center">
+    <div className="fixed font-serif italic inset-0 grid place-content-center space-y-8 text-center text-lg text-stone-500">
       Loading...
     </div>
   )
